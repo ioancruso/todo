7df61270c827984fa7b66c265d2f6b3050839a15
Crușoveanu Ioan <ionut.cruso@gmail.com>
2023-07-10 13:11:56 +0300

    favicon

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,15 @@
 import Header from '../sections/header/header'
 import Footer from '../sections/footer/footer'
 
+import Head from 'next/head'
+
 import { ThemeProvider } from "next-themes";
 import { useState } from 'react'
 
 import { SessionContextProvider } from '@supabase/auth-helpers-react'
 import { supabase } from '@/utilities/supabase'
+
+import Logo from '@/svgs/logo';
 
 import "../styles/base.scss";
 
@@ -14,6 +18,9 @@ export default function MyApp({ Component, pageProps }) {
     const [supabaseClient] = useState(() => supabase)
 
     return <>
+        <Head>
+            <link rel="icon" href="/favicon.svg" sizes="any" />
+        </Head>
         <SessionContextProvider
             supabaseClient={supabaseClient}
             initialSession={pageProps.initialSession}
